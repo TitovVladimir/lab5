fd99635e5230e4f0fd1853271d73d7381effaca7
TitovVladimir <chazzii1337@gmail.com>
2018-04-25 19:56:31 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,74 @@
-var gulp = require('gulp'),
-  uglify = require('gulp-uglify');
-  uglifycss = require('gulp-cssmin');
-  copy = require('gulp-contrib-copy'); 
-  rename = require("gulp-rename");
-  browserSync = require('browser-sync');
-  reload = browserSync.reload;
-  paths = {
-    html:['index.html'],
-    css:['css/style.css'],
-    script:['js/selectAuto.js']
-  };
-  
-  
-gulp.task('minifyjs', function () {
-  gulp.src('js/selectAuto.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('build/js'))
-});
-  
-gulp.task('minifycss', function () {
-  gulp.src('css/style.css')
-  .pipe(uglifycss())
-  .pipe(gulp.dest('build/css'))
+
+
+var gulp = require('gulp'), // Сообственно Gulp JS
+    uglify = require('gulp-uglify'), // Минификация JS
+    concat = require('gulp-concat'), // Склейка файлов
+    imagemin = require('gulp-imagemin'), // Минификация изображений
+    csso = require('gulp-csso'); // Минификация CSS
+	browserSync = require('browser-sync');
+    reload = browserSync.reload;
+	paths = {
+      html:['src/index.html'],
+      css:['src/style/style.css'],
+      script:['src/js/selectAuto.js']
+    };
+	
+// Задача "sass". Запускается командой "gulp sass"
+gulp.task('sass', function () { 
+	gulp.src('src/style/style.css') // файл, который обрабатываем
+		.pipe(csso()) // минифицируем css, полученный на предыдущем шаге
+		.pipe(gulp.dest('build/style/')); // результат пишем по указанному адресу
 });
 
-gulp.task('copyhtml', function() {
-  gulp.src('index.html')
-  .pipe(copy())
-  .pipe(gulp.dest('build'));
+// Задача "js". Запускается командой "gulp js"
+gulp.task('js', function() {
+    gulp.src([
+                'src/js/jquery-3.2.1.min.js',
+                'src/js/selectAuto.js'
+        ]) // файлы, которые обрабатываем
+   //     .pipe(concat('min.js')) // склеиваем все JS
+        .pipe(uglify()) // получившуюся "портянку" минифицируем 
+        .pipe(gulp.dest('build/js')) // результат пишем по указанному адресу
 });
 
-gulp.task('gulpall', function () {
-  gulp.src('js/selectAuto.js')
-  .pipe(uglify())
-  .pipe(rename({
-    basename: "selectAuto",
-    suffix: ".min",
-    extname: ".js"
-  }))
-  .pipe(gulp.dest('build/js'))
-  gulp.src('css/style.css')
-  .pipe(uglifycss())
-  .pipe(rename({
-    basename: "style",
-    suffix: ".min",
-    extname: ".css"
-  }))
-  .pipe(gulp.dest('build/css'))
-  gulp.src('index.html')
-  .pipe(copy())
-  .pipe(gulp.dest('build'));
+// Задача "images". Запускается командой "gulp images"
+gulp.task('images', function() {
+    gulp.src('src/img/*') // берем любые файлы в папке и ее подпапках
+        .pipe(imagemin()) // оптимизируем изображения для веба
+        .pipe(gulp.dest('build/img/')) // результат пишем по указанному адресу
+
 });
 
-gulp.task('html', function(){
+gulp.task('html', function () { 
+	gulp.src('src/index.html') // файл, который обрабатываем
+		.pipe(gulp.dest('build/')); // результат пишем по указанному адресу
+});
+
+gulp.task('build', ['sass', 'images', 'html', 'js']);
+
+// Задача "watch". Запускается командой "gulp watch"
+// Она следит за изменениями файлов и автоматически запускает другие задачи
+gulp.task('watch', function () {
+	// При изменение файлов *.scss в папке "styles" и подпапках запускаем задачу sass
+	gulp.watch('src/style/**/*.css', ['sass']); 
+	// При изменение файлов *.js папке "javascripts" и подпапках запускаем задачу js
+	gulp.watch('src/js/**/*.js', ['js']);
+	// При изменение любых файлов в папке "images" и подпапках запускаем задачу images
+	gulp.watch('src/img/**/*', ['images']);
+	gulp.watch('src/**/*.html', ['html']); 
+});
+
+gulp.task('html1', function(){
   gulp.src(paths.html)
   .pipe(reload({stream:true}));
 });
 
-gulp.task('css', function(){
+gulp.task('css1', function(){
   gulp.src(paths.css)
   .pipe(reload({stream:true}));
 });
 
-gulp.task('js', function(){
+gulp.task('js1', function(){
   gulp.src(paths.script)
   .pipe(reload({stream:true}));
 });
@@ -70,7 +76,7 @@ gulp.task('js', function(){
 gulp.task('browserSync', function() {
   browserSync({
     server: {
-      baseDir: "./"
+      baseDir: "src/"
     },
     port: 8080,
     open: true,
@@ -79,9 +85,9 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('watcher',function(){
-  gulp.watch(paths.css, ['css']);
-  gulp.watch(paths.script, ['js']);
-  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.css, ['css1']);
+  gulp.watch(paths.script, ['js1']);
+  gulp.watch(paths.html, ['html1']);
 });
 
-gulp.task('default', ['watcher', 'browserSync']);
+gulp.task('dev', ['watcher', 'browserSync', 'watch']);
---
+++ src/js/selectAuto.js
@@ -0,0 +1,102 @@
+﻿window.onload = function() {
+  var markSelect = $('.mark_select');
+  var modelSelect = $('.model_select');
+  var modelCheck = $('.model_select').val();
+  var yearSelect = $('.year_select');
+  var markCheck = $('.mark_select').val();
+  var yearCheck = $('.year_select').val();
+  var disSelect = $('.unDis');
+  var disButton = $('.reset_button');
+  var cars = 
+    [{img: "img/bmw3.webp", mark: "BMW", model: "3er", year: "2013" },
+	{img: "img/rs6.jpg", mark: "Audi", model: "rs6", year: "2017" },
+	{img: "img/suberb.jpg", mark: "Scoda", model: "Superb", year: "2017" },
+	{img: "img/golf.jpg", mark: "VW", model: "Golf 7 GTI", year: "2015" },
+	{img: "img/mazda6.jpg", mark: "Mazda", model: "6", year: "2008" }];
+  var loadMarks = function() {
+	var allMarks = "";
+	for (var i=0; cars[i] != undefined; i++) {
+	  if (cars[i+1] != undefined) {
+	    allMarks = allMarks + "'<option>"+cars[i].mark+"</option>'+";
+	  } else {
+		  allMarks = allMarks + "'<option>"+cars[i].mark+"</option>';";  
+	  }
+	};
+    var markSelectUpdate =
+    '<option hidden>Выберите марку</option>' +
+    allMarks;	
+    markSelect.append(markSelectUpdate);
+  }
+  var loadContent = function() {
+	var contentBlock = $('#car_info');
+	contentBlock.empty();
+	var numberCar = 0;
+	markCheck = $('.mark_select').val();
+  modelCheck = $('.model_select').val();
+  yearCheck = $('.year_select').val();
+	for (var i=0; cars[i] != undefined; i++) {
+	  if (cars[i].mark == markCheck) {
+		  if (cars[i].model == modelCheck) {
+		    if (cars[i].year == yearCheck) {
+			    numberCar = i;
+		    }
+		  }
+	  }
+  }
+	var newInfo = 
+	'<h1><img src="'+cars[numberCar].img+'" alt=""></h1>' +
+	'<h2>Марка: '+cars[numberCar].mark+'</h2>' +
+	'<h2>Модель: '+cars[numberCar].model+'</h2>' +
+	'<h2>Год выпуска: '+cars[numberCar].year+'</h2>';
+	contentBlock.append(newInfo);
+	disButton.empty();
+	disButton.append('<input type="submit" value="Сброс!">');
+  }
+  $('.mark_select').change(function() {
+	disSelect.empty();
+	var unDisabled =
+	'<select class="model_select">' +
+    '</select>' +
+    '<select class="year_select" disabled>' +              
+    '</select>';
+	disSelect.append(unDisabled);
+    var allModels = "";
+	markCheck = $('.mark_select').val();
+	for (var i=0; cars[i] != undefined; i++) {
+	  if (cars[i].mark == markCheck) {
+	    allModels = allModels + "'<option>"+cars[i].model+"</option>'+";
+	  } 
+	};
+    var modelSelectUpdate =
+    '<option hidden>Выберите модель</option>' +
+    allModels;	
+    modelSelect = $('.model_select');
+    modelSelect.append(modelSelectUpdate);
+	$('.model_select').change(function() {
+	  disSelect.empty();
+	  var unDisabled =
+	  '<select class="model_select">' +
+	  allModels +
+      '</select>' +
+      '<select class="year_select">' +              
+      '</select>';
+      disSelect.append(unDisabled);
+      var allYears = "";
+	  modelCheck = $('.model_select').val();
+	  for (i=0; cars[i] != undefined; i++) {
+	    if (cars[i].model == modelCheck) {
+	      allYears = allYears + "'<option>"+cars[i].year+"</option>'+";
+	    } 
+	  };
+      var yearSelectUpdate =
+      '<option hidden>Выберите год</option>' +
+      allYears;	
+      yearSelect = $('.year_select');
+      yearSelect.append(yearSelectUpdate);
+	  $('.year_select').change(function() {
+		loadContent();
+	  })//loadContent
+    })//loadYears
+  })//loadModels
+  loadMarks();
+}	
